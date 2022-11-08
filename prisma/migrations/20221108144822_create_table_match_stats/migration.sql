cae431ce88ee1def1fcad3f4c6eeb9bd99c08d19
liverday <vitor.medeiro10@gmail.com>
2022-11-08 11:49:05 -0300

    feat: include model match statistics

---
+++ prisma/migrations/20221108144822_create_table_match_stats/migration.sql
@@ -0,0 +1,33 @@
+-- CreateTable
+CREATE TABLE "MatchStats" (
+    "teamId" TEXT NOT NULL,
+    "matchId" TEXT NOT NULL,
+    "attempsOnGoal" INTEGER NOT NULL,
+    "kicksOnTarget" INTEGER NOT NULL,
+    "kicksOffTarget" INTEGER NOT NULL,
+    "kicksBlocked" INTEGER NOT NULL,
+    "kicksOnWoodwork" INTEGER NOT NULL,
+    "corners" INTEGER NOT NULL,
+    "offsides" INTEGER NOT NULL,
+    "ballPossession" INTEGER NOT NULL,
+    "passAccuracy" INTEGER NOT NULL,
+    "passes" INTEGER NOT NULL,
+    "passesCompleted" INTEGER NOT NULL,
+    "distanceCovered" INTEGER NOT NULL,
+    "ballsRecovered" INTEGER NOT NULL,
+    "tackles" INTEGER NOT NULL,
+    "clearances" INTEGER NOT NULL,
+    "yellowCards" INTEGER NOT NULL,
+    "redCards" INTEGER NOT NULL,
+    "foulsCommited" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "MatchStats_pkey" PRIMARY KEY ("teamId","matchId")
+);
+
+-- AddForeignKey
+ALTER TABLE "MatchStats" ADD CONSTRAINT "MatchStats_teamId_fkey" FOREIGN KEY ("teamId") REFERENCES "Team"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "MatchStats" ADD CONSTRAINT "MatchStats_matchId_fkey" FOREIGN KEY ("matchId") REFERENCES "Match"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
